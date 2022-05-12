49efc8dd5466e262aeb4621da547b8116259a103
quigiaosu103 <93850311+quigiaosu103@users.noreply.github.com>
2022-05-12 20:57:40 +0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,67 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "Main character0":return img`
-.....f..........
-..fff1fffff.....
-..f11111111f....
-.f1111111111f...
-f1111111bb11f...
-.ffbbbbbbb11f...
-..fdfdddfb1df...
-..fdfdddfbbbf...
-..fddddddbbf....
-...feeeeeef.....
-...fffffffef....
-...feeefeeef....
-..ffeeffeeedf...
-.fdfeeeeeefdf...
-.fffbbbcccfff...
-...fbbbbccf.....
-...fbf.fccf.....
-....ff..fff.....
-...fff..ff......
-................
-`;
-            case "7[-=fLG=Q7OK+WS{0Q2I":
-            case "Main character":return img`
-. . . . . . f f f f f f f f . . 
-. . . . . f 1 1 1 1 1 1 1 1 f . 
-. . . . 1 1 1 1 1 1 1 1 1 1 1 f 
-. . . . 1 1 1 1 b b 1 1 1 1 f f 
-. . . . f d 1 b f b b b f b f . 
-. . . . f b b b f d d d f d f . 
-. . . . . f b b d d d d d d f . 
-. . . . . . f e e e e e e f . . 
-. . . . . f e f f f f f f f . . 
-. . . . . f b e e f e e e f . . 
-. . . . f d b b e f f e e f f . 
-. . . . f d f b e e e e e f d f 
-. . . . f f f c c c b b b f f f 
-. . . . . . f c c b f f b . . . 
-. . . . . . . f f . . f b . . . 
-. . . . . . . f f . . f f . . . 
-`;
-            case "image2":
-            case "Main character1":return img`
-. . . . . . f f f f f f f f . . 
-. . . . . f 1 1 1 1 1 1 1 1 f . 
-. . . . f 1 1 1 1 1 1 1 1 1 1 1 
-. . . . f f 1 1 1 1 b b 1 1 1 1 
-. . . . . f b f b b b f b 1 d f 
-. . . . . f d f d d d f b b b f 
-. . . . . f d d d d d d b b f . 
-. . . . . . f e e e e e e f . . 
-. . . . . . f f f f f f f e f . 
-. . . . . . f e e e f e e b f . 
-. . . . . f f e e f f e b b d f 
-. . . . f d f e e e e e b f d f 
-. . . . f f f b b b c c c f f f 
-. . . . . . . b f f b c c f . . 
-. . . . . . . b f . . f f . . . 
-. . . . . . . f f . . f f . . . 
-`;
             case "image3":
             case "Main character2":return img`
 . . . . . . f f f f f f f f . . 
@@ -101,6 +40,118 @@ f1111111bb11f...
 . . . . . . . f c c b f f b . . 
 . . . . . . . . f c . . f b . . 
 . . . . . . . . f f . . f f . . 
+`;
+            case "7[-=fLG=Q7OK+WS{0Q2I":
+            case "Main character":return img`
+. . . . . . f f f f f f f f . . 
+. . . . . f 1 1 1 1 1 1 1 1 f . 
+. . . . 1 1 1 1 1 1 1 1 1 1 1 f 
+. . . . 1 1 1 1 b b 1 1 1 1 f f 
+. . . . f d 1 b f b b b f b f . 
+. . . . f b b b 3 d d d f d f . 
+. . . . . f b b d d d d d d f . 
+. . . . . . f e e e e e e f . . 
+. . . . . f e f f f f f f f . . 
+. . . . . f b e e f e e e f . . 
+. . . . f d b b e f f e e f f . 
+. . . . f d f b e e e e e f d f 
+. . . . f f f c c c b b b f f f 
+. . . . . . f c c b f f b . . . 
+. . . . . . . f f . . f b . . . 
+. . . . . . . f f . . f f . . . 
+`;
+            case "image6":
+            case "Main character5":return img`
+. . . . . . f f f f f f f f . . 
+. . . . . f 1 1 1 1 1 1 1 1 f . 
+. . . . 1 1 1 1 1 1 1 1 1 1 1 f 
+. . . . 1 1 1 1 b b 1 1 1 1 f f 
+. . . . f d 1 b f b b b f b f . 
+. . . . f b b b f d d d f d f . 
+. . . . . f b b d d d d d d f . 
+. . . . . . f e e e e e e f . . 
+. . . . . f e f f f f f f f . . 
+. . . . . f b e e f e e e f . . 
+. . . . f d b b e f f e e f f . 
+. . . . f d f b e e e e e f d f 
+. . . . f f f c c c b b b f f f 
+. . . . . 3 f c c b f f b . . . 
+. . . . . . 3 3 3 3 f b . . . . 
+. . . . . . . f f 3 3 f . . . . 
+`;
+            case "image2":
+            case "Main character1":return img`
+..............bbbbbbb...........
+...........bb66663333baa........
+.........bb3367776333663aa......
+........b33333888333389633aa....
+.......b3333333333333389633aa...
+......b34443333333333338633bae..
+.....b3455433333333334443333ae..
+....b33322333dddd3333455233daee.
+...b3d333333dd3bbbb33322333dabe.
+..b3d333333d3bb33bb33333333da4e.
+..bd33333333b33aab3333333223a4ee
+.b3d3663333b33aab33366332442b4ee
+.bd3b983333a3aa3333387633ee3b4ee
+.bd6983333baaa333333387633bb4bee
+b3d6833333bba333333333863ba44ebe
+bdd3333333bb3333333333333a44bebe
+add666633333322333366333ba44bbbe
+ad67776333332442336983d3a444b4e.
+add888b333333ee3369833d3a44b44e.
+add333333333333336833d3a444b4e..
+a3dd3333344433333dddd3a444b44e..
+ab33ddd325543333dd33aa444b44e...
+.eabb3dd32233333baaa4444b44e....
+.ebabb3d333d33baa444443b44e.....
+..ebaab3ddd3aaa4444433b44e......
+..eebbaab33a44444333b444e.......
+...eeebbaab444b333b4444e........
+....ebeeebbbbbbbb4444ee.........
+.....eebbbb44444444ee...........
+.......eeebbb444eee.............
+..........eeeeee................
+................................
+`;
+            case "image1":
+            case "Main character0":return img`
+. . . . . f f f 1 f f f f f . . 
+. . . . . f 1 1 1 1 1 1 1 1 f . 
+. . . . f 1 1 1 1 1 1 1 1 1 1 f 
+. . . f 1 1 1 1 1 1 1 b b 1 1 f 
+. . . . f f b b b b b b b 1 1 f 
+. . . . . f d f d d d f b 1 d f 
+. . . . . f d f d d d f b b b f 
+. . . . . f d d d d d d b b f . 
+. . . . . . f e e e e e e f . . 
+. . . . . . f f f f f f f e f . 
+. . . . . . f e e e f e e e f . 
+. . . . . f f e e f f e e e d f 
+. . . . f d f e e e e e e f d f 
+. . . . f f f b b b c c c f f f 
+. . . . . . f b b b b c c f . . 
+. . . . . . f b f . f c c f . . 
+. . . . . . . f f . . f f f . . 
+`;
+            case "image5":
+            case "Main character4":return img`
+. . . . . f f f 1 f f f f f . . 
+. . . . . f 1 1 1 1 1 1 1 1 f . 
+. . . . f 1 1 1 1 1 1 1 1 1 1 f 
+. . . f 1 1 1 1 1 1 1 b b 1 1 f 
+. . . . f f b b b b b b b 1 1 f 
+. . . . . f d f d d d f b 1 d f 
+. . . . . f d f d d d f b b b f 
+. . . . . f d d d d d d b b f . 
+. . . . . . f e e e e e e f . . 
+. . . . . . f f f f f f f e f . 
+. . . . . . f e e e f e e e f . 
+. . . . . f f e e f f e e e d f 
+. . . . f d f e e e e e e f d f 
+. . . . f f f b b b c c c f f f 
+. . . . . . f b f . f c c f . . 
+. . . . . . f b f . . c f f . . 
 `;
         }
         return null;

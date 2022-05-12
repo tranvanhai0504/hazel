8f854cf1e82749b99da30879c37562be8fa8f96c
tranvanhai0504 <94392136+tranvanhai0504@users.noreply.github.com>
2022-05-12 16:21:10 +0700

    Add new main charater

--- main.ts
+++ main.ts
@@ -200,22 +200,26 @@ scene.setBackgroundImage(img`
     `)
 tiles.setCurrentTilemap(tilemap`level1`)
 mySprite = sprites.create(img`
-    . . . . . . . f f f f f . . . . 
-    . . . . . . f e e e e e f . . . 
-    . . . . f f e e e d d d d f . . 
-    . . . f d d e e d d d d d d c . 
-    . . . c d b e e d f d d f d c . 
-    f f . c d b e e d f d d f d d c 
-    f e f . c f e e d d d d e e d c 
-    f e f . . f e e e d c d d d d c 
-    f e f . . f f e e e d c c c f . 
-    f e f . f e e e e f f f f f . . 
-    . f f f e e e e e e e f . . . . 
-    . . f e e e e f e e f e f f . . 
-    . . f e e e f f f e e f f e f . 
-    . f b f f f f f f c d d b d d f 
-    . f d d c f . . f d d d c d d f 
-    . . f f f . . . f f f f f f f . 
+    ..........f.....
+    .....fffffefff..
+    ....feeeeeeeef..
+    ...feeeeeeeeeef.
+    ...feebbeeeeeeef
+    ...feebbbbbbbff.
+    ...fdebfdddfdf..
+    ...fbbbfdddfdf..
+    ....fbbddddddf..
+    .....feeeeeef...
+    ....fefffffff...
+    ....feeefeeef...
+    ...fdeeeffeeff..
+    ...fdfeeeeeefdf.
+    ...fffcccbbbfff.
+    .....fccbbbbf...
+    .....fccf.fbf...
+    .....fff..ff....
+    ................
+    ................
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
 scene.cameraFollowSprite(mySprite)
